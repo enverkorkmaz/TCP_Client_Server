935baaf22aa64d405cd9d6fa7235be6a8c73c58c
Enver Korkmaz <enverkorkmaz17@gmail.com>
2023-02-21 20:28:03 +0300

    21.02.23

---
+++ ClientServer/Client/Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        try
+        {
+            //IP adresi ve port numarasını tanımlama
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            int port = 8888;
+
+            //TcpClient oluşturma ve Servera bağlanma
+            TcpClient client = new TcpClient();
+            client.Connect(ipAddress, port);
+
+            //NetworkStream
+            NetworkStream stream = client.GetStream();
+
+            //Server'a mesaj gönderme
+            string message = "Hello";
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            stream.Write(buffer, 0, buffer.Length);
+            Console.WriteLine("Client: " + message);
+
+            //Server'dan gelen veriyi okuma
+            byte[] serverBuffer = new byte[1024];
+            int serverBytesRead = stream.Read(serverBuffer, 0, serverBuffer.Length);
+            string serverMessage = Encoding.ASCII.GetString(serverBuffer, 0, serverBytesRead);
+            Console.WriteLine("Server: " + serverMessage);
+
+            //TcpClient bağlantısını kapatma
+            client.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Hata", e);
+        }
+
+        Console.WriteLine("\n Client kapatıldı.");
+        Console.ReadLine();
+    }
+}
+
---
+++ ClientServer/Server/Server.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        TcpListener server = null;
+
+        try
+        {
+            //IP adresi ve port numarası tanımlama
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            int port = 8888;
+
+            //TcpListener başlatma
+            server = new TcpListener(ipAddress, port);
+            server.Start();
+            Console.WriteLine("Server başlatıldı.");
+
+            //Client bağlantısını kabul etme
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Client bağlandı.");
+
+            //NetworkStream
+            NetworkStream stream = client.GetStream();
+
+            //Client'tan gelen veriyi okuma
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string clientMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Client: " + clientMessage);
+
+            //Server'dan Client'a veri gönderme
+            string serverMessage = clientMessage + DateTime.Now.ToString();
+            byte[] message = Encoding.ASCII.GetBytes(serverMessage);
+            stream.Write(message, 0, message.Length);
+            Console.WriteLine("Server: " + serverMessage);
+
+            // Client bağlantısını kapatma
+            client.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Hata", e);
+        }
+        finally
+        {
+            // TcpListener'i kapatma
+            server.Stop();
+        }
+
+        Console.WriteLine("\nServer kapatıldı.");
+        Console.ReadLine();
+    }
+}
